e33f955f6dcd6f70b42c0370bf9e8e5ce6de7816
Lex <lex@lextatic.com>
2024-06-10 22:51:16 -0300

    Reviewed TagName code

--- src/Runtime/TagName.cs
+++ src/Runtime/TagName.cs
@@ -1,89 +1,157 @@
-﻿namespace GameplayTags.Runtime
+namespace GameplayTags.Runtime;
+
+/// <summary>
+/// An immutable and memory optimized <see cref="string"/> for representing a <see cref="TagName"/>. The class keeps a
+/// list of all previously created <see cref="TagName"/> objects and always returns the same instance when a
+/// <see cref="TagName"/> is requested. All instantiations should be handled by the <see langword="static"/> method
+/// <see cref="FromString(string)"/>.
+/// </summary>
+public readonly struct TagName : IComparable, IComparable<TagName>, IEquatable<TagName>
 {
-	public readonly struct TagName : IComparable, IComparable<TagName>, IEquatable<TagName>
+	private static readonly Dictionary<string, TagName> _nameTable = new ();
+
+	private readonly string _name;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="TagName"/> struct.
+	/// <para>Do not use this constructor!! Use the <see langword="static"/> method <see cref="FromString(string)"/>
+	/// instead.</para>
+	/// </summary>
+	[Obsolete($"Use {nameof(TagName)}.FromString() instead.", true)]
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Info Code Smell", "S1133:Deprecated code should be removed", Justification = "This is as intended.")]
+	public TagName()
 	{
-		private static readonly Dictionary<string, TagName> _nameTable = new Dictionary<string, TagName>();
-
-		private readonly string _name;
-
-		[Obsolete("Use TagName.FromString() instead.", true)]
-		public TagName()
-		{
-			_name = string.Empty;
-		}
-
-		private TagName(string name)
-		{
-			_name = name;
-			_nameTable.Add(name, this);
-		}
-
-		public static TagName FromString(string name)
-		{
-			name = name.ToLower();
-			if (_nameTable.TryGetValue(name, out var fName))
-			{
-				return fName;
-			}
-
-			return new TagName(name);
-		}
-
-		public static bool operator ==(TagName a, TagName b) { return a.Equals(b); }
-		public static bool operator !=(TagName a, TagName b) { return !a.Equals(b); }
-		public static bool operator <(TagName a, TagName b) { return a.CompareTo(b) < 0; }
-		public static bool operator >(TagName a, TagName b) { return a.CompareTo(b) > 0; }
-		public static bool operator <=(TagName a, TagName b) { return a.CompareTo(b) <= 0; }
-		public static bool operator >=(TagName a, TagName b) { return a.CompareTo(b) >= 0; }
-
-		public override string ToString()
-		{
-			return _name;
-		}
-
-		public override int GetHashCode()
-		{
-			return _name.GetHashCode();
-		}
-
-		public override bool Equals(object? obj)
-		{
-			if (obj == null)
-			{
-				return false;
-			}
-
-			if (obj is not TagName tagName)
-			{
-				throw new ArgumentException("Compared object must be of type TagName.");
-			}
-
-			return _name.Equals(tagName._name);
-		}
-
-		public bool Equals(TagName other)
-		{
-			return _name.Equals(other._name);
-		}
-
-		public int CompareTo(object? obj)
-		{
-			if (obj == null)
-			{
-				return 1;
-			}
-
-			if (obj is not TagName tagName)
-			{
-				throw new ArgumentException("Compared object must be of type TagName.");
-			}
-
-			return CompareTo(tagName);
-		}
-
-		public int CompareTo(TagName other)
-		{
-			return string.Compare(_name, other._name, StringComparison.InvariantCultureIgnoreCase);
-		}
+		_name = string.Empty;
 	}
+
+	private TagName(string name)
+	{
+		_name = name;
+		_nameTable.Add(name, this);
+	}
+
+	/// <summary>
+	/// Returns an existing instance of <see cref="TagName"/> based on a <see langword="string"/> or creates a new one
+	/// if it doesn't already exist.
+	/// </summary>
+	/// <param name="name">Name of the desired <see cref="TagName"/>.</param>
+	/// <returns>A valid <see cref="TagName"/>.</returns>
+	public static TagName FromString(string name)
+	{
+		name = name.ToLower();
+		if (_nameTable.TryGetValue(name, out var fName))
+		{
+			return fName;
+		}
+
+		return new TagName(name);
+	}
+
+	/// <summary>
+	/// Returns a <see cref="string"/> representation of the <see cref="TagName"/>.
+	/// </summary>
+	/// <returns>The <see cref="TagName"/>'s name as a <see cref="string"/>.</returns>
+	public override string ToString()
+	{
+		return _name;
+	}
+
+	/// <summary>
+	/// Returns the hash code for this <see cref="TagName"/>.
+	/// </summary>
+	/// <returns>A 32-bit signed integer hash code.</returns>
+	public override int GetHashCode()
+	{
+		return _name.GetHashCode();
+	}
+
+	/// <summary>
+	/// Determines wether this instance and another specified <see cref="TagName"/> object have the same value.
+	/// </summary>
+	/// <param name="obj">The other <see cref="object"/> to compare against.</param>
+	/// <returns><see langword="true"/> if the value of the <paramref name="obj"/> parameter is the same as the value of
+	/// this instance; otherwise, <see langword="false"/>. If <paramref name="obj"/> is <see langword="null"/>, the
+	/// method returns <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentException">Compared object must be of type <see cref="TagName"/>.</exception>
+	public override bool Equals(object? obj)
+	{
+		if (obj is null)
+		{
+			return false;
+		}
+
+		if (obj is not TagName tagName)
+		{
+			return false;
+		}
+
+		return _name.Equals(tagName._name);
+	}
+
+	/// <summary>
+	/// Determines wether this instance and another specified <see cref="TagName"/> object have the same value.
+	/// </summary>
+	/// <param name="other">The other <see cref="TagName"/> to compare against.</param>
+	/// <returns><see langword="true"/> if the value of the <paramref name="other"/> parameter is the same as the value of
+	/// this instance; otherwise, <see langword="false"/>. If <paramref name="other"/> is <see langword="null"/>, the
+	/// method returns <see langword="false"/>.</returns>
+	public bool Equals(TagName other)
+	{
+		return _name.Equals(other._name);
+	}
+
+	/// <summary>
+	/// Compares two specified <see cref="TagName"/> objects using
+	/// <see cref="StringComparison.InvariantCultureIgnoreCase"/>, and returns an integer that indicates their relative
+	/// position in the sort orther.
+	/// </summary>
+	/// <param name="obj">The other <see cref="object"/> to compare against.</param>
+	/// <returns>A 32-bit signed integer that indicates the lexical relationship between the two comparands.
+	/// <para><b>Less than zero</b> - this instance precedes <paramref name="other"/> in the sort order.</para>
+	/// <para><b>Zero</b> - this instance is the same position as <paramref name="other"/> in the sort order.</para>
+	/// <para><b>Greater than zero</b> - this instance follows <paramref name="other"/> in the sort order.</para>
+	/// </returns>
+	/// <exception cref="ArgumentException">Compared object must be of type <see cref="TagName"/>.</exception>
+	public int CompareTo(object? obj)
+	{
+		if (obj is null)
+		{
+			return 1;
+		}
+
+		if (obj is not TagName tagName)
+		{
+			throw new ArgumentException($"Compared object must be of type {nameof(TagName)}.");
+		}
+
+		return CompareTo(tagName);
+	}
+
+	/// <summary>
+	/// Compares two specified <see cref="TagName"/> objects using
+	/// <see cref="StringComparison.InvariantCultureIgnoreCase"/>, and returns an integer that indicates their relative
+	/// position in the sort orther.
+	/// </summary>
+	/// <param name="other">The other <see cref="TagName"/> to compare against.</param>
+	/// <returns>A 32-bit signed integer that indicates the lexical relationship between the two comparands.
+	/// <para><b>Less than zero</b> - this instance precedes <paramref name="other"/> in the sort order.</para>
+	/// <para><b>Zero</b> - this instance is the same position as <paramref name="other"/> in the sort order.</para>
+	/// <para><b>Greater than zero</b> - this instance follows <paramref name="other"/> in the sort order.</para>
+	/// </returns>
+	public int CompareTo(TagName other)
+	{
+		return string.Compare(_name, other._name, StringComparison.InvariantCultureIgnoreCase);
+	}
+
+	public static bool operator ==(TagName a, TagName b) { return a.Equals(b); }
+
+	public static bool operator !=(TagName a, TagName b) { return !a.Equals(b); }
+
+	public static bool operator <(TagName a, TagName b) { return a.CompareTo(b) < 0; }
+
+	public static bool operator >(TagName a, TagName b) { return a.CompareTo(b) > 0; }
+
+	public static bool operator <=(TagName a, TagName b) { return a.CompareTo(b) <= 0; }
+
+	public static bool operator >=(TagName a, TagName b) { return a.CompareTo(b) >= 0; }
 }
