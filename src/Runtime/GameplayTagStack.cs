8eac154cf71e3c723e2ef7f3a65cef1d0f2033c1
Lex <lex@lextatic.com>
2024-06-16 22:48:09 -0300

    Naive TagStack implementation

--- src/Runtime/GameplayTagStack.cs
+++ src/Runtime/GameplayTagStack.cs
@@ -1,19 +1,37 @@
-﻿namespace GameplayTags.Runtime
+namespace GameplayTags.Runtime;
+
+/// <summary>
+/// An implementation of the <see cref="GameplayTag"/> that supports stacks.
+/// </summary>
+public struct GameplayTagStack
 {
-	public struct GameplayTagStack
+	/// <summary>
+	/// Gets the <see cref="GameplayTag"/> for this tag stack.
+	/// </summary>
+	public GameplayTag Tag { get; }
+
+	/// <summary>
+	/// Gets the number of stacks for this <see cref="GameplayTagStack"/>.
+	/// </summary>
+	public int Count { get; internal set; }
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="GameplayTagStack"/> struct.
+	/// </summary>
+	/// <param name="tag">The <see cref="GameplayTag"/> for this stack.</param>
+	/// <param name="count">The number of initial stacks.</param>
+	public GameplayTagStack(GameplayTag tag, int count = 1)
 	{
-		public GameplayTag Tag { get; private set; }
-		public int Count { get; private set; }
+		Tag = tag;
+		Count = count;
+	}
 
-		public GameplayTagStack(GameplayTag tag, int count)
-		{
-			Tag = tag;
-			Count = count;
-		}
-
-		public override string ToString()
-		{
-			return Tag.ToString();
-		}
+	/// <summary>
+	/// Returns a <see cref="string"/> representation of the <see cref="GameplayTagStack"/>.
+	/// </summary>
+	/// <returns>The <see cref="GameplayTagStack"/> as a <see cref="string"/>.</returns>
+	public override string ToString()
+	{
+		return $"{Tag}:{Count}";
 	}
 }
--- src/Runtime/GameplayTagStackContainer.cs
+++ src/Runtime/GameplayTagStackContainer.cs
@@ -1,32 +1,83 @@
-﻿namespace GameplayTags.Runtime
+using System;
+
+namespace GameplayTags.Runtime;
+
+/// <summary>
+/// A <see cref="GameplayTagStackContainer"/> holds a collection of <see cref="GameplayTagStack"/>s.
+/// </summary>
+public readonly struct GameplayTagStackContainer
 {
-	public struct GameplayTagStackContainer
+	private readonly HashSet<GameplayTagStack> _stacks = new ();
+
+	private readonly Dictionary<GameplayTag, int> _tagCountMap = new ();
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="GameplayTagStackContainer"/> struct.
+	/// </summary>
+	public GameplayTagStackContainer()
 	{
-		private HashSet<GameplayTagStack> _stacks = new();
-		private Dictionary<GameplayTag, int> _tagCountMap = new();
+	}
 
-		public GameplayTagStackContainer()
+	/// <summary>
+	/// Adds a certain ammount of stacks from the given tag.
+	/// </summary>
+	/// <param name="tag">The tag to remove stacks from.</param>
+	/// <param name="count">The amount of stacks to remove.</param>
+	public void AddStackCount(GameplayTag tag, int count)
+	{
+		if (count > 0)
 		{
-		}
+			var modifiedStack = _stacks.Single(x => x.Tag == tag);
 
-		public void AddStackCount(GameplayTag tag, int count)
+			// What if it doesn't exist in the _stacks yet?
+			modifiedStack.Count += count;
+			_tagCountMap[tag] = modifiedStack.Count;
+		}
+	}
+
+	/// <summary>
+	/// Removes a certain ammount of stacks from the given tag.
+	/// </summary>
+	/// <param name="tag">The tag to remove stacks from.</param>
+	/// <param name="count">The amount of stacks to remove.</param>
+	public void RemoveStackCount(GameplayTag tag, int count)
+	{
+		// Should check if contains first?
+		if (count > 0)
 		{
+			var modifiedStack = _stacks.Single(x => x.Tag == tag);
 
+			if (modifiedStack.Count <= count)
+			{
+				_stacks.Remove(modifiedStack);
+				_tagCountMap.Remove(tag);
+			}
+			else
+			{
+				modifiedStack.Count -= count;
+				_tagCountMap[tag] = modifiedStack.Count;
+			}
 		}
+	}
 
-		public void RemoveStackCount(GameplayTag tag, int count)
-		{
+	/// <summary>
+	/// Check whether this StackContainer has a given tag.
+	/// </summary>
+	/// <param name="tag">Tag to check.</param>
+	/// <returns>True if contains, false otherwise.</returns>
+	public readonly bool HasTag(GameplayTag tag)
+	{
+		return _tagCountMap.ContainsKey(tag);
+	}
 
-		}
-
-		public bool HasTag(GameplayTag tag)
-		{
-			return _tagCountMap.ContainsKey(tag);
-		}
-
-		public int GetStackCount(GameplayTag tag)
-		{
-			return _tagCountMap[tag];
-		}
+	/// <summary>
+	/// Gets the stack count for a given tag.
+	/// </summary>
+	/// <param name="tag">Tag to check.</param>
+	/// <returns>Number of stacks for the given tag.</returns>
+	public readonly int GetStackCount(GameplayTag tag)
+	{
+		// What if it doesn't contain the tag?
+		return _tagCountMap[tag];
 	}
 }
