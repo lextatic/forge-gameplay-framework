4f163cc153303ba2f11eb66f3180910a35b45bb0
Lex <lex@lextatic.com>
2024-06-23 20:20:56 -0300

    Added more TagName tests

--- src/Runtime.Tests/TagNameTests.cs
+++ src/Runtime.Tests/TagNameTests.cs
@@ -16,34 +16,41 @@ public class TagNameTests
 
 	[TestMethod]
 	[TestCategory("Equality")]
+	public void TagName_from_empty_string_is_Empty_tag_name()
+	{
+		var tagName1 = TagName.FromString(string.Empty);
+
+		Assert.IsTrue(tagName1 == TagName.Empty);
+	}
+
+	[TestMethod]
+	[TestCategory("Equality")]
 	public void TagNames_are_equatable()
 	{
 		var tagNameA = TagName.FromString("tagNameA");
 		var tagNameB = TagName.FromString("tagnameb");
 		var tagNameC = TagName.FromString("tagnamea");
 
-		Assert.IsTrue(tagNameA.CompareTo(tagNameC) == 0);
-
 		Assert.IsTrue(tagNameA != tagNameB);
 		Assert.IsTrue(tagNameA == tagNameC);
 		Assert.IsTrue(tagNameA.Equals(tagNameC));
 
-		object tagObjectA = tagNameA;
-		object tagObjectB = tagNameB;
-		object tagObjectC = tagNameC;
+		object tagNameObjectA = tagNameA;
+		object tagNameObjectB = tagNameB;
+		object tagNameObjectC = tagNameC;
 
-		Assert.IsTrue(tagNameA.Equals(tagObjectA));
-		Assert.IsTrue(tagNameA.Equals(tagObjectC));
-		Assert.IsTrue(tagObjectA.Equals(tagObjectC));
+		Assert.IsTrue(tagNameA.Equals(tagNameObjectA));
+		Assert.IsTrue(tagNameA.Equals(tagNameObjectC));
+		Assert.IsTrue(tagNameObjectA.Equals(tagNameObjectC));
 
-		Assert.IsTrue((TagName)tagObjectA == (TagName)tagObjectC);
+		Assert.IsTrue((TagName)tagNameObjectA == (TagName)tagNameObjectC);
 
 		// Those are defaul C# object == and !=, not the overriden ones.
 		// So they should be considered different objects
-		Assert.IsTrue(tagObjectA != tagObjectB);
-		Assert.IsTrue(tagObjectA != tagObjectC);
-		Assert.IsFalse(tagObjectA == tagObjectB);
-		Assert.IsFalse(tagObjectA == tagObjectC);
+		Assert.IsTrue(tagNameObjectA != tagNameObjectB);
+		Assert.IsTrue(tagNameObjectA != tagNameObjectC);
+		Assert.IsFalse(tagNameObjectA == tagNameObjectB);
+		Assert.IsFalse(tagNameObjectA == tagNameObjectC);
 	}
 
 	[TestMethod]
@@ -54,6 +61,7 @@ public class TagNameTests
 		var tagNameB = TagName.FromString("tagnameb");
 		var tagNameC = TagName.FromString("tagnamea");
 
+		Assert.IsTrue(tagNameA.CompareTo(tagNameC) == 0);
 		Assert.IsTrue(tagNameA.CompareTo(tagNameB) < 0);
 		Assert.IsTrue(tagNameB.CompareTo(tagNameA) > 0);
 
@@ -63,5 +71,53 @@ public class TagNameTests
 		Assert.IsTrue(tagNameA >= tagNameC);
 		Assert.IsTrue(tagNameA <= tagNameB);
 		Assert.IsTrue(tagNameB >= tagNameC);
+	}
+
+	[TestMethod]
+	[TestCategory("Comparability")]
+	public void TagNames_are_always_higher_than_Empty_tag_name()
+	{
+		var tagNameA = TagName.FromString("tagNameA");
+		var tagNameB = TagName.FromString("tagnameb");
+		var tagNameC = TagName.FromString("tagnamea");
+
+		Assert.IsTrue(TagName.Empty.CompareTo(tagNameA) < 0);
+		Assert.IsTrue(TagName.Empty.CompareTo(tagNameB) < 0);
+		Assert.IsTrue(TagName.Empty.CompareTo(tagNameC) < 0);
+
+		Assert.IsTrue(tagNameA.CompareTo(TagName.Empty) > 0);
+		Assert.IsTrue(tagNameB.CompareTo(TagName.Empty) > 0);
+		Assert.IsTrue(tagNameC.CompareTo(TagName.Empty) > 0);
+
+		Assert.IsTrue(TagName.Empty.CompareTo(TagName.Empty) == 0);
+
+		Assert.IsTrue(tagNameA > TagName.Empty);
+		Assert.IsTrue(tagNameB > TagName.Empty);
+		Assert.IsTrue(tagNameA > TagName.Empty);
+		Assert.IsTrue(TagName.Empty < tagNameA);
+		Assert.IsTrue(TagName.Empty < tagNameB);
+		Assert.IsTrue(TagName.Empty < tagNameC);
+
+		Assert.IsTrue(TagName.Empty == TagName.Empty);
+		Assert.IsTrue(TagName.Empty >= TagName.Empty);
+		Assert.IsTrue(TagName.Empty <= TagName.Empty);
+	}
+
+	[TestMethod]
+	[TestCategory("ToString")]
+	public void TagNames_ToString_always_returns_lowercase_string()
+	{
+		var tagName1 = TagName.FromString("Hello World");
+		var tagName2 = TagName.FromString("HELLO TAGS");
+
+		Assert.IsTrue(string.Compare(tagName1.ToString(), "hello world", false) == 0);
+		Assert.IsTrue(string.Compare(tagName2.ToString(), "hello tags", false) == 0);
+	}
+
+	[TestMethod]
+	[TestCategory("ToString")]
+	public void Empty_tag_name_ToString_returns_empty_string()
+	{
+		Assert.IsTrue(string.Compare(TagName.Empty.ToString(), string.Empty) == 0);
 	}
 }
